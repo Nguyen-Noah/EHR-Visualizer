8b887c345631e64f95bbc15dc9f0553e32121860
Austin Decker <deckaj01@pfw.edu>
2024-10-20 10:54:40 -0400

    testing mongodb error-handling.

--- experiments/Austin/FHIR-EHR-API/db_functions/connectionSingleton.js
+++ experiments/Austin/FHIR-EHR-API/db_functions/connectionSingleton.js
@@ -6,7 +6,22 @@ const DataBaseSingleton = {
     init: async function(){
         if(!client){
 
-            client = await MongoClient.connect(process.env.MONGO_DB, {maxPoolSize: 10});     
+            client = await MongoClient.connect(process.env.MONGO_DB, {maxPoolSize: 10});
+
+            client.on("connectionCreated", ()=>{
+                console.log("connected to database");
+            })
+            client.on("error", (error) =>{
+                console.error(error.message);
+            });
+            
+            client.on("timeout", ()=>{
+                console.error("Mongodb connection timeout.");
+            });
+
+            client.on("connectionPoolClosed", ()=>{
+                console.log("database connection pool closed.")
+            });
         }
         return client;
     },
