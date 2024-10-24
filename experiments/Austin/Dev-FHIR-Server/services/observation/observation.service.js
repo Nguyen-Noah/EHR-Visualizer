cab8c716c3e58efafcb12bcda0b112d910065528
Austin Decker <deckaj01@pfw.edu>
2024-10-24 10:31:27 -0400

    syncing with repo.

--- experiments/Austin/Dev-FHIR-Server/services/observation/observation.service.js
+++ experiments/Austin/Dev-FHIR-Server/services/observation/observation.service.js
@@ -1,5 +1,9 @@
 const {DatabaseSingleton} = require("../../connectionSingleton");
 
-module.exports.search = async (args, context) => {
+module.exports.search = async(args, context) => {
     throw new Error("Unable to locate observations");
 };
+
+module.exports.searchById = async(args, context) => {
+    throw new Error("Unable to locate observations");
+}
--- experiments/Austin/Dev-FHIR-Server/services/patient/patient.service.js
+++ experiments/Austin/Dev-FHIR-Server/services/patient/patient.service.js
@@ -1,5 +1,11 @@
 const {DatabaseSingleton} = require("../../connectionSingleton");
 
 module.exports.search = async(args, context) => {
+    
     throw new Error("Unable to locate patients");
 };
+
+module.exports.searchById = async(args, context) => {
+    throw new Error("Unable to locate patients");
+}
+
